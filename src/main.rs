cbe9d10a1f228d1ff14f150e5caf7528687b9d66
Yazan Salameh <yazan@serenbit.ps>
2023-07-22 16:43:57 +0300

    Init launch settings for rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn print_list(list: &Vec<u32>) {
+    for number in list {
+        print!("{}\t", number);
+    }
+	
+    println!("");
+}
+
 fn main() {
+    let item = vec![1, 2, 3];
+
+    print_list(&item);
+
     println!("Hello, world!");
 }
